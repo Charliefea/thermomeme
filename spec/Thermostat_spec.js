3edc95d6df992dcf313d1a2a9d1697d6a65bef8b
jade <jadealvares@hotmail.co.uk>
2017-06-07 11:21:16 +0100

    first half of test

---
+++ spec/Thermostat_spec.js
@@ -0,0 +1,19 @@
+describe('thermostat', function() {
+  var
+});
+
+// describe('airport', function() {
+//   var airport;
+//   beforeEach(function(){
+//     airport = new Airport();
+//   });
+//
+//   describe('initialize airport', function (){
+//     it('initializes with the capacity', function(){
+//       airport2 = new Airport(40)
+//       expect(airport2.capacity).toEqual(40);
+//     });
+//     it('without capacity given, initializes with default', function(){
+//       expect(airport.capacity).toEqual(20);
+//     });
+//   });
